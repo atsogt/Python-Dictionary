d264c8c9d16f10876d3079e4a5a428c865c3a97e
Anand Tsogtbaatar <atsogt@artic.edu>
2020-04-22 14:25:20 -0400

    third commit

--- dictionary.py
+++ dictionary.py
@@ -7,8 +7,12 @@ data = json.load(open('data.json'))
 
 def translate(w):
     w = w.lower()
-    if word in data:
+    if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input('Did you mean %s instead? Press Y for yes or press N for no: ' %
                    get_close_matches(w, data.keys())[0])
